629f4b3c49b3b9de3b6d1f12eb89c41b7b20777a
Felipe Junges <felipejunges@outlook.com>
2019-04-19 11:46:08 -0300

    AutoIncremento no Entity

--- Formula1.Data/Entities/Entity.cs
+++ Formula1.Data/Entities/Entity.cs
@@ -1,8 +1,12 @@
-﻿namespace Formula1.Data.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Formula1.Data.Entities
 {
     public class Entity
     {
-        // TODO: autoincremento
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
     }
 }
