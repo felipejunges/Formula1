61c8ab09770894b11eb14bd7eef2fb8a8f00522f
Felipe Junges <felipejunges@yahoo.com.br>
2021-08-29 22:33:27 -0300

    ✨ Migration Resultado-DSQ

--- Formula1.Infra/Migrations/20210830013131_Resultado-DSQ.Designer.cs
+++ Formula1.Infra/Migrations/20210830013131_Resultado-DSQ.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Formula1.Infra.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Formula1.Infra.Migrations
 {
     [DbContext(typeof(F1Db))]
-    partial class F1DbModelSnapshot : ModelSnapshot
+    [Migration("20210830013131_Resultado-DSQ")]
+    partial class ResultadoDSQ
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Formula1.Infra/Migrations/20210830013131_Resultado-DSQ.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Formula1.Infra.Migrations
+{
+    public partial class ResultadoDSQ : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "DSQ",
+                table: "Resultados",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Corridas",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .Annotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "NumeroCorrida",
+                table: "Corridas",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DSQ",
+                table: "Resultados");
+
+            migrationBuilder.DropColumn(
+                name: "NumeroCorrida",
+                table: "Corridas");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Corridas",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int))
+                .OldAnnotation("Sqlite:Autoincrement", true);
+        }
+    }
+}
