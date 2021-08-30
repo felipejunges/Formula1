c430ee5a8a786bf085610c748622c1197f6715f0
Felipe Junges <felipejunges@yahoo.com.br>
2021-08-29 22:37:09 -0300

    wip

---
+++ Formula1.Infra/Migrations/20210830013626_Corrida-MetadePontos.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Formula1.Infra.Migrations
+{
+    public partial class CorridaMetadePontos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Pontos",
+                table: "Resultados",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Pontos",
+                table: "PilotosTemporada",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Pontos",
+                table: "EquipesTemporada",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "MetadePontos",
+                table: "Corridas",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MetadePontos",
+                table: "Corridas");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Pontos",
+                table: "Resultados",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(decimal));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Pontos",
+                table: "PilotosTemporada",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(decimal));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Pontos",
+                table: "EquipesTemporada",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(decimal));
+        }
+    }
+}
