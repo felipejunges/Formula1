4cdab70e98ec552d06a82193652b8262950dc794
Felipe Junges <felipe.junges@cwi.com.br>
2022-01-05 22:36:52 -0300

    ✨ Atualizado para .Net 6

--- Formula1.MVC/Models/LoginViewModel.cs
+++ Formula1.MVC/Models/LoginViewModel.cs
@@ -2,8 +2,8 @@
 {
     public class LoginViewModel
     {
-        public string Email { get; set; }
+        public string? Email { get; set; }
 
-        public string Senha { get; set; }
+        public string? Senha { get; set; }
     }
 }
